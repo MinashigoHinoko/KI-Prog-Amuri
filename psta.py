9dba7770b15a1516add992cdabbd102f47032fe0
Hiko <amir.amuri@yahoo.de>
2022-01-22 15:10:27 +0100

    Update psta.py

--- psta.py
+++ psta.py
@@ -25,59 +25,46 @@ def printExpr(inp):
 def evalExpr(inp, env):
 
 """
-"""
-The following are examples of uses of these functions:
->>> Expr.printExpr("x = y")
-(x = y)
->>> Expr.printExpr("x + 2 * y")
-(x + (2 * y))
->>> Expr.printExpr("x < 2 and y < 1")
-((x < 2) and (y < 1))
->>> Expr.printExpr("(x + 2*y < 15 + x * x) or z = 5")
-(((x + (2 * y)) < (15 + (x * x))) or (z = 5))
->>> Expr.printExpr("x + 2*y < 15 + x * x or z = 5")
-(((x + (2 * y)) < (15 + (x * x))) or (z = 5))
-"""
-"""
->>> env = {’x’:1, ’y’:2, ’z’:3}
->>> Expr.evalExpr("x = y", env)
-False
->>> Expr.evalExpr("x + 2 * y", env)
-5
->>> Expr.evalExpr("x < 2 and y < 1", env)
-False
->>> Expr.evalExpr("(x + 2*y < 15 + x * x) or z = 5", env)
-True
->>> Expr.evalExpr("x + 2*y < 15 + x * x or z = 5", env)
-True
->>> Expr.evalExpr("x * 2 + 3 < x * (2 + 3)", env)
-False
->>> Expr.evalExpr("y * 2 + 3 < y * (2 + 3)", env)
-True
-"""
 class Expr:
     
     def printExpr(inp):
+        """
+        The following are examples of uses of these functions:
+        >>> Expr.printExpr("x = y")
+        (x = y)
+        >>> Expr.printExpr("x + 2 * y")
+        (x + (2 * y))
+        >>> Expr.printExpr("x < 2 and y < 1")
+        ((x < 2) and (y < 1))
+        >>> Expr.printExpr("(x + 2*y < 15 + x * x) or z = 5")
+        (((x + (2 * y)) < (15 + (x * x))) or (z = 5))
+        >>> Expr.printExpr("x + 2*y < 15 + x * x or z = 5")
+        (((x + (2 * y)) < (15 + (x * x))) or (z = 5))
+        """
         prnt = se.ParseExpr().parse(inp)
         print(pcomb.result(prnt))
-        pass
-    
-    def evalExpr(inp, env):
-        pass
-    
-class BoolExpr:
-    
-    def printExpr(inp):
-        pass
         
-    def evalExpr(inp, env):
-        pass
-    
-class ArithmExpr:
-    
-    def printExpr(inp):
-        pass
     
     def evalExpr(inp, env):
+        """
+        >>> env = {’x’:1, ’y’:2, ’z’:3}
+        >>> Expr.evalExpr("x = y", env)
+        False
+        >>> Expr.evalExpr("x + 2 * y", env)
+        5
+        >>> Expr.evalExpr("x < 2 and y < 1", env)
+        False
+        >>> Expr.evalExpr("(x + 2*y < 15 + x * x) or z = 5", env)
+        True
+        >>> Expr.evalExpr("x + 2*y < 15 + x * x or z = 5", env)
+        True
+        >>> Expr.evalExpr("x * 2 + 3 < x * (2 + 3)", env)
+        False
+        >>> Expr.evalExpr("y * 2 + 3 < y * (2 + 3)", env)
+        True
+        """
+        evl = se.ParseExpr().parse(inp)
+        z = type(evl[0][0].toZ3())
+        print(pcomb.result(evl).ev(env))
         pass
 
